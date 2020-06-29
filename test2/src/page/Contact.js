4709bbaa4ae4c06c0dc5942daad5cec753578e12
quynhkhuong <quynhkt62@wru.vn>
2020-06-29 10:25:18 +0700

    add

---
+++ test2/src/page/Contact.js
@@ -0,0 +1,11 @@
+import React , {Component}from 'react'
+
+export default class Contact extends Comment {
+    render(){
+        return(
+            <>
+            <h1>Contact</h1>
+            </>
+        )
+    }
+}
---
+++ test2/src/page/Home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Home extends Comment {
+  render(){
+    return(
+      <>
+        <h1>Home</h1>
+      </>
+    )
+  }
+}
